826c4fddbdefae4e9e0e6e1e80264cfb75a26a3d
omaomach <aaronjoash44@gmail.com>
2025-10-02 16:06:55 +0300

    worked on match_case conditional

--- 08_conditionals/04_match_case.py
+++ 08_conditionals/04_match_case.py
@@ -1,11 +1,11 @@
-a = 26
+a = int(input("Enter a number between 1 and 10: "))
 
 match a:
-    case 122:
-        print("the value is 122")
+    case 1:
+        print("You won a charger")
     case 2:
-        print("the value is 2")
+        print("You won $2")
     case 6:
-        print("the value is 6")
+        print("You won a camera")
     case _:
         print("Better luck next time")
