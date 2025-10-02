bf7bab3c291fe6616b17aa15348dced8c33aade4
omaomach <aaronjoash44@gmail.com>
2025-10-03 01:13:36 +0300

    worked on the pass statement

--- 09_loops/05_continue.py
+++ 09_loops/05_continue.py
@@ -1,4 +1,4 @@
-for i in range(0, 21):
+for i in range(1, 21):
     if i == 11:
-        break
+        continue # The continue statement skips the rest of the code in the current iteration and moves to the next iteration
     print(i)
---
+++ 09_loops/06_pass.py
@@ -0,0 +1,5 @@
+i = 3
+
+if i == 12:
+    pass # The pass statement is a placeholder that does nothing. It is used when syntax requires a statement but no action is needed
+print("End of program")
