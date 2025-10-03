956e9401bdb1ef281fa033a271250cc5a35322de
omaomach <aaronjoash44@gmail.com>
2025-10-04 02:57:34 +0300

    worked on String creation and String indexing

---
+++ 10_strings/01_string.py
@@ -0,0 +1,12 @@
+first_name = "Joash"
+second_name = "Omao"
+description = '''
+Joash
+is
+a
+software engineer
+'''
+
+# The (''' ''') is used for multy line strings and its also used for comments in Python.
+
+print(description)
