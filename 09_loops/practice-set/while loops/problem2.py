234eee73b737fa1aab43cf27168fe242c3b43084
omaomach <aaronjoash44@gmail.com>
2025-10-04 01:37:07 +0300

    provided alternative solution for while loops problem2

--- 09_loops/practice-set/while loops/problem2.py
+++ 09_loops/practice-set/while loops/problem2.py
@@ -1,9 +1,16 @@
 correctpwd = "joashomao"
 
-while True:
-    pwd = input(f"Enter a password: ")
-    if pwd == correctpwd:
-        print("Access granted")
-        break
-    else:
-        print("Incorrect password. Try again.")
+# while True:
+#     pwd = input(f"Enter a password: ")
+#     if pwd == correctpwd:
+#         print("Access granted")
+#         break
+#     else:
+#         print("Incorrect password. Try again.")
+
+entered_pwd = input("Enter password \n")
+
+while entered_pwd != correctpwd:
+    entered_pwd = input("Incorrect! Please try again \n")
+
+print("Access Granted")
