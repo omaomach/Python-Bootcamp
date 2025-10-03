5e146518f776c45467445b2b65f67ba41235d3c0
omaomach <aaronjoash44@gmail.com>
2025-10-04 02:04:37 +0300

    provided alternative solution to while loop

--- 09_loops/practice-set/break, continue and pass statements/problem2.py
+++ 09_loops/practice-set/break, continue and pass statements/problem2.py
@@ -1,7 +1,12 @@
-num = 0
+# num = 0
 
-while num < 10:
-    num += 1
-    if num == 5:
+# while num < 10:
+#     num += 1
+#     if num == 5:
+#         continue
+#     print(num)
+
+for i in range(1, 11):
+    if i == 5:
         continue
-    print(num)
+    print(i)
