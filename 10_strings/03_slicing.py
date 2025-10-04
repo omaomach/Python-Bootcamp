7b854b46f14609b3a4e8441ab97e033d9f99adc0
omaomach <aaronjoash44@gmail.com>
2025-10-04 03:18:41 +0300

    worked on string slicing

---
+++ 10_strings/03_slicing.py
@@ -0,0 +1,12 @@
+name = "Joashomaomachoka"
+
+# print(name[0:2]) # goes from 0 to (n-1) i.e 0->(2-1)
+
+# print(name[2:-1]) # Same as name [2:4]
+
+# print(name[0:10:n]) # skip n- 1 characters
+
+print(name[0:10:3]) # skip 3-1 characters i.e 2 characters
+
+print(name[:4]) # Replace the first empty number with 0 i.e name[0:4]
+print(name[1:]) # Replace the second empty number with length i.e name[1:15]
