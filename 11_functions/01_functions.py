9fcaa189db89023cbd57b11fc246bf0ad5f03ce2
omaomach <aaronjoash44@gmail.com>
2025-10-06 22:57:44 +0300

    working on functions

---
+++ 11_functions/01_functions.py
@@ -0,0 +1,20 @@
+# a = 4
+# b = 2
+# c = 1
+
+# average = (a + b + c)/3.0
+# print(average)
+
+# a1 = 6
+# b1 = 7
+# c1 = 12
+
+# average1 = (a1 + b1 + c1)/3
+# print(average1)
+
+def average(a, b, c):
+    solution = (a + b + c)/3
+    print(solution)
+
+average(3, 5, 1)
+
