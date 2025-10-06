265c8e80932ac4aa067e4e4426a4118485151cfd
omaomach <aaronjoash44@gmail.com>
2025-10-06 23:47:47 +0300

    worked on lambda functions

---
+++ 11_functions/03_lambda.py
@@ -0,0 +1,19 @@
+square = lambda x: x*x
+'''
+As good as writing
+def square(x):
+    return x * x
+
+'''
+
+sum = lambda x, y: x + y
+'''
+As good as writing
+
+def sum(x, y):
+    return x + y
+
+'''
+
+print(square(3))
+print(sum(4, 6))
