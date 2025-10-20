97535f6eba789e40ebdef1678e1f36e6628d5f77
omaomach <aaronjoash44@gmail.com>
2025-10-21 01:21:40 +0300

    worked on python modules and virtual environments

---
+++ 11_functions/05_modules.py
@@ -0,0 +1,19 @@
+# We have 2 types of modules in Python:
+# - Built in modules
+# - External modules - Can either be written by you or be installed using a utility called pip
+# - https://docs.python.org/3/py-modindex.html - List of all built in python modules
+
+import math
+import os
+import mymodule
+import requests # used to fetch the html of online pages
+import pandas
+
+print(math.sqrt(8))
+mymodule.hello()
+r = requests.get('https://www.comraid.io')
+# print(r.text)
+h = r.headers['content-type']
+print(h)
+
+# os.abort()
