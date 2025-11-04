9c2ad46fde1e8130078fdc4fede2e6b1d3dc607a
omaomach <aaronjoash44@gmail.com>
2025-11-05 02:45:44 +0300

    worked on Docstrings

---
+++ 11_functions/08_docstrings.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    """Returns the sum of two numbers.""" # Docstrings are used to document functions, classes and modules.
+    return a + b
+
+print(add.__doc__)
+
+def mul(a, b):
+    """
+    Returns the product of two numbers.
+
+    parameters:
+    a (int): The first argument.
+    b (int): The second argument.
+
+    returns:
+    int: The product of two numbers.
+    """
+
+    return a * b
