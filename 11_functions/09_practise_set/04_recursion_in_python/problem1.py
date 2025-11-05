bd2aaaeac52df3b17b233d4f5af96a5f6724b85c
omaomach <aaronjoash44@gmail.com>
2025-11-05 05:51:49 +0300

    worked on using external libraries

--- 11_functions/09_practise_set/04_recursion_in_python/problem1.py
+++ 11_functions/09_practise_set/04_recursion_in_python/problem1.py
@@ -4,4 +4,4 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-print(factorial(0))
+print(factorial(10))
---
+++ 11_functions/09_practise_set/05_modules_and_pip_using_external_libraries/problem1.py
@@ -0,0 +1,10 @@
+import math
+
+def square_root(n):
+    return  math.sqrt(n)
+
+print(square_root(25))
+
+angle_degrees = 90
+angle_radians = math.radians(angle_degrees)
+print(math.sin(angle_radians))
