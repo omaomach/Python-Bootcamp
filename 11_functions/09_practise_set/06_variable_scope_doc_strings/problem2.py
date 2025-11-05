18f68dface455bbd3e0d87b7897884a4bbc9b2bb
omaomach <aaronjoash44@gmail.com>
2025-11-05 07:30:20 +0300

    worked on 11 09 06 doc strings

---
+++ 11_functions/09_practise_set/06_variable_scope_doc_strings/problem2.py
@@ -0,0 +1,16 @@
+def multiply(a, b):
+    """
+    Returns the product of 2 numbers
+
+    parameters:
+    a (int): The first number
+    b (int): The second number
+
+    returns:
+    int: The product of two numbers
+
+    """
+
+    return a * b
+
+help(multiply)
