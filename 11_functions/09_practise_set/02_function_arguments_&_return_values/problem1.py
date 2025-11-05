3c3c72d854cecc94684f47c42fe2c06eac2b8cd3
omaomach <aaronjoash44@gmail.com>
2025-11-05 03:17:40 +0300

    worked on function arguments and return values practice set

---
+++ 11_functions/09_practise_set/02_function_arguments_&_return_values/problem1.py
@@ -0,0 +1,4 @@
+def full_name(first, last):
+    return first + " " + last
+
+print(full_name("Joash", "Omao"))
---
+++ 11_functions/09_practise_set/02_function_arguments_&_return_values/problem2.py
@@ -0,0 +1,5 @@
+def calculate_area(length, width=7):
+    return length * width
+
+print(calculate_area(10, 5))
+print(calculate_area(8))
