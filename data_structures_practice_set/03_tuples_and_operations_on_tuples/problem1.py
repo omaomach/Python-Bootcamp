04a6dd746492ceaf41655e2cc8070352473bc987
omaomach <aaronjoash44@gmail.com>
2025-11-13 17:14:49 +0300

    worked on problem 1 tuples and operations on tuples

---
+++ data_structures_practice_set/03_tuples_and_operations_on_tuples/problem1.py
@@ -0,0 +1,12 @@
+coordinates = (10, 20)
+
+print(coordinates)
+
+# coordinates[0] = 50 # Tuples dont support object assigment or modification
+print(coordinates)
+
+coordinates_list = list(coordinates)
+coordinates_list[0] = 50
+
+coordinates = tuple(coordinates_list)
+print(coordinates)
