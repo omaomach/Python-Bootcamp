7bc7e3a1c6431bc0092dd358fea9929081c12e17
omaomach <aaronjoash44@gmail.com>
2026-01-01 04:33:54 +0300

    went through inheritance

--- 16_oop/04_inheritance.py
+++ 16_oop/04_inheritance.py
@@ -1,3 +1,8 @@
+### Inheritance ###
+# It's like a family tree. A child class or subclass inherits traits (attributes and methods) from
+# its parent class (or superclass)
+# This allows you to create new classes that are specialized versions of existing classes, without rewriting all the code
+
 class Animal:
     location = "Australia"
 
@@ -7,7 +12,7 @@ class Animal:
     def speak(self):
         print("Speaking now...")
 
-class Dog(Animal):
+class Dog(Animal): # The Dog class is extending the Animal class and reusing its methods and attributes
     def speak(self):
         super().speak() # Inside the child class, super() lets you call methods from the parent class.
         # This is useful when you want to extend the parent's behaviour instead of completely replacing it.
