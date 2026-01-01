14ddec9597dde97e43ae91378d0d8f3c25b1b178
omaomach <aaronjoash44@gmail.com>
2026-01-01 05:12:26 +0300

    worked on method overriding and operator overloading

--- 16_oop/05_operator_overloading.py
+++ 16_oop/05_operator_overloading.py
@@ -3,7 +3,18 @@ class Point:
         self.x = x
         self.y = y
 
-    def
+    def sum(self, p):
+        return Point((self.x + p.x), (self.y + p.y))
 
-point1 = Point(10, 20)
-point2 = Point(30, 10)
+    def print_point(self):
+        print(f"x is {self.x}, y is {self.y}")
+
+    def __add__(self, p):
+        return Point((self.x + p.x), (self.y + p.y))
+
+p1 = Point(10, 20)
+p2 = Point(30, 10)
+
+# p = p1.sum(p2) # Returns a new point which is sum of p1 and p2
+p = p1 + p2 # We overloaded the + operator by writing __add__ function
+p.print_point()
