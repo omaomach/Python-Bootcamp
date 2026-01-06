b3f11033e8737b8a0233d6c260f5cf5e23a31e2a
omaomach <aaronjoash44@gmail.com>
2026-01-07 01:42:16 +0300

    worked on static, class and instance methods

--- 17_python_advanced_concepts/04_instance_static_class.py
+++ 17_python_advanced_concepts/04_instance_static_class.py
@@ -9,9 +9,20 @@ class Employee:
         info = f"Name: {self.name}\nSalary: {self.salary}"
         print(info)
 
+    # Static Method
     @staticmethod
     def sum(a, b, c): # This is a method that doesn't require self and self is not automatically passed when the function is called. Used for righting utility methods
         return a  + b + c
+
+    # Class Methods
+    @classmethod
+    def print_company(cls):
+        company = cls.company
+        print(company)
+
+    @classmethod
+    def change_company(cls, new_company):
+        cls.company = new_company
 
 e1 = Employee("Bridget", 100000)
 e2 = Employee("Kris", 200000)
@@ -21,3 +32,10 @@ e1.print_info()
 e2.print_info()
 
 print(e1.sum(2,3,5))
+
+e2.print_company()
+e2.change_company("Lenovo")
+e2.print_company()
+print(Employee.company) # The class variable has been changed
+
+# Whenever an attribute is called, inside an object, its first searched within its instance attributes, if not found, it's searched in the class attributes
