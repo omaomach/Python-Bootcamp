1b9fb6c00d91de3d139d6c8f052205ae3f7f510c
omaomach <aaronjoash44@gmail.com>
2026-01-07 03:16:45 +0300

    working on raising errors

--- 17_python_advanced_concepts/errors.py
+++ 17_python_advanced_concepts/errors.py
@@ -1,3 +1,5 @@
+### Basic Exception Handling ###
+"""
 while True:
     try:
         a = int(input("Enter a number 1: "))
@@ -15,3 +17,18 @@ while True:
 
     except Exception as e:
         print("Something went wrong", e) # Whenever there is an error, the code inside the except block is run
+"""
+
+
+### Raising Exceptions ###
+a = int(input("Enter a number: "))
+if a == 0:
+    raise ValueError("Don't input a zero even though it is a valid operation")
+
+b = int(input("Enter another number: "))
+if b == 0:
+    raise ZeroDivisionError("You can't divide by zero.")
+
+quotient = a / b
+
+print(f"{a}/{b} is {quotient}")
