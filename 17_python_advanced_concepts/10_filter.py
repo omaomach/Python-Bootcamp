bdab9566f68e43bea0de422c2011fcb7650cff9a
omaomach <aaronjoash44@gmail.com>
2026-01-11 00:20:05 +0300

    worked on reduce() higher order function

--- 17_python_advanced_concepts/10_filter.py
+++ 17_python_advanced_concepts/10_filter.py
@@ -1,8 +1,8 @@
-def is_greater_than_9(x):
-    if x > 9:
-        return True
-    else:
-        return False
+# def is_greater_than_9(x):
+#     if x > 9:
+#         return True
+#     else:
+#         return False
 
 numbers = [1, 2, 3, 5, 6, 10, 13, 14 ,78, 100, 8, 112, 200]
 
