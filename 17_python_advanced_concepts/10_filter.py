948a65762281e34aad3802259b1c81f138bc0c96
omaomach <aaronjoash44@gmail.com>
2026-01-11 00:09:16 +0300

    created the lambda version of the is_greater_than_9 function

--- 17_python_advanced_concepts/10_filter.py
+++ 17_python_advanced_concepts/10_filter.py
@@ -6,6 +6,6 @@ def is_greater_than_9(x):
 
 numbers = [1, 2, 3, 5, 6, 10, 13, 14 ,78, 100, 8, 112, 200]
 
-new_numbers = filter(is_greater_than_9, numbers)
+new_numbers = filter(lambda x: x > 9, numbers) # the is_greater_than_9 method is individually run on all the numbers in the "numbers array". When new returns True, "new_numbers" will collect that number
 
-print(list(new_numbers))
+print(list(new_numbers)) # type cast the filter object to a list then print
