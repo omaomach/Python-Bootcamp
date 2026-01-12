63838801c6d9a71a7312c77abb6e02f0c0d0a0d7
omaomach <aaronjoash44@gmail.com>
2026-01-12 04:00:56 +0300

    worked on decorator practice set 2

---
+++ 17_python_advanced_concepts/practice_set/decorators/p_set_2.py
@@ -0,0 +1,15 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} took {time.time() - start} seconds")
+        return result
+    return wrapper
+
+@timer
+def calculate_sum(n):
+    return sum(range(n))
+
+calculate_sum(1000000)
