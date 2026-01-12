eb11d7470eced07e00025d70f0c9f1a836ed7134
omaomach <aaronjoash44@gmail.com>
2026-01-12 03:36:49 +0300

    simplified for loop in kwargs example

--- 17_python_advanced_concepts/14_kwargs.py
+++ 17_python_advanced_concepts/14_kwargs.py
@@ -1,5 +1,6 @@
 def marks(**kwargs): # kwargs is a dictionary with all the key value pairs which were passed to marks
-    for item in kwargs.keys():
-        print(f"The marks of {item}, is {kwargs[item]}")
+    # for item in kwargs.keys():
+    for name, score in kwargs.items(): # Simpler version of the for loop
+        print(f"The marks of {name}, is {score}")
 
 marks(joshua=90, joel=87, joash=77)
