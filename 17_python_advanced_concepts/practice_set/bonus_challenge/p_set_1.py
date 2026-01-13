6b11164896d2c8e15399de16cc30af0e7c54194b
omaomach <aaronjoash44@gmail.com>
2026-01-13 20:24:15 +0300

    worked on bonus challenge practice set 1

---
+++ 17_python_advanced_concepts/practice_set/bonus_challenge/p_set_1.py
@@ -0,0 +1,22 @@
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Calling function(s)...")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+@decorator
+def add(a, b):
+    return a + b
+
+@decorator
+def subtract(a, b):
+    return a - b
+
+@decorator
+def multiply(a, b):
+    return a * b
+
+print(add(1, 2))
+print(subtract(1, 2))
+print(multiply(12, 2))
