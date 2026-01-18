4a9a7641241abee8ea6f94211d43fb071331bfcb
omaomach <aaronjoash44@gmail.com>
2026-01-18 23:26:13 +0300

    working with files

---
+++ 18_files/01_read.py
@@ -0,0 +1,7 @@
+f = open("joash.txt", "r")
+
+content = f.read()
+
+print(content)
+
+f.close()
---
+++ 18_files/01_write.py
@@ -0,0 +1,15 @@
+# Write to a file called John Doe.txt
+# The file should contain data about John Doe
+# You need to note that when using write "w" for writing. If the file exists, its
+# contents will be overwritten. If the file doesn't exist, a new one will be created.
+
+f = open('John Doe.txt', 'w')
+
+string = '''
+John Doe is a nice guy. He lives in NYC and he works with Python.
+God willing, I'll also go abroad.
+'''
+
+f.write(string)
+
+f.close()
