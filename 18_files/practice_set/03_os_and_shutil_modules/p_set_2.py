8fd2ea6e36c8807472ea47c2cfcc0a8216e39ab8
omaomach <aaronjoash44@gmail.com>
2026-02-25 15:26:13 +0300

    worked on moving and deleting files

--- 18_files/practice_set/03_os_and_shutil_modules/p_set_2.py
+++ 18_files/practice_set/03_os_and_shutil_modules/p_set_2.py
@@ -5,3 +5,9 @@ import os
 os.makedirs("backup", exist_ok=True)
 shutil.copy("file1.txt", "backup/file1.txt")
 
+# 2. Move a file to a new folder
+os.makedirs("archive", exist_ok=True)
+shutil.move("file2.txt", "archive/file2.txt")
+
+# 3. Delete a file (irreversible!)
+os.remove("archive/file2.txt")
