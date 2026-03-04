e6f9ff51745fd331dd06334c67d05452f7081528
omaomach <aaronjoash44@gmail.com>
2026-03-04 19:25:03 +0300

    correcting practice sets

--- 18_files/practice_set/03_os_and_shutil_modules/p_set_2.py
+++ 18_files/practice_set/03_os_and_shutil_modules/p_set_2.py
@@ -7,7 +7,7 @@ shutil.copy("file1.txt", "backup/file1.txt")
 
 # 2. Move a file to a new folder
 os.makedirs("archive", exist_ok=True)
-shutil.move("file2.txt", "archive/file2.txt")
+# shutil.move("file2.txt", "archive/file2.txt")
 
 # 3. Delete a file (irreversible!)
 os.remove("archive/file2.txt")
