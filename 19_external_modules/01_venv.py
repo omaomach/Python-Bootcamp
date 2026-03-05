b0b8ecbec5e4f909a8d1c6d162961030cd3954f4
omaomach <aaronjoash44@gmail.com>
2026-03-05 16:04:20 +0300

    worked on virtual environments and package management

---
+++ 19_external_modules/01_venv.py
@@ -0,0 +1,19 @@
+# https://pypi.org/project/virtualenv/ - Install virtualenv utility
+
+# python -m venv python_course_venv - create the virtual env
+
+# source virtual_env_name/bin/activate or .\python_course_venv\Scripts\Activate.ps1 - Activate a virtual environment
+
+# pip list -  create a copy of all the packages installed in the virtual environment
+
+# pip freeze > requirements.txt - copy the packages to a file
+
+# pip install -r requirements.txt - install the packages from the file
+
+# pip install --upgrade package_name - upgrade a certain package
+
+# pip uninstall package_name - uninstalling a package
+
+# deactivate
+
+import moviepy
