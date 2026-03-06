593a21285d5f72b7cafb2c4f0151e1d22c4e9e5f
omaomach <aaronjoash44@gmail.com>
2026-03-06 04:35:41 +0300

    updated code

--- 19_external_modules/02_requests.py
+++ 19_external_modules/02_requests.py
@@ -2,5 +2,11 @@ import requests # This is a package that can get you html, txt, or JSON data fro
 
 r = requests.get('https://api.github.com/users/omaomach')
 
+if r.status_code == 200:
+    data = r.json()
+    print(data['name'])
+else:
+    print(f"Error: {r.status_code}")
+
 with open ("omaomach.txt", "w") as f:
     f.write(r.text)
