d3763b68b1fbebe0796eee569d7dcb0a4821ee79
omaomach <aaronjoash44@gmail.com>
2026-03-10 20:27:40 +0300

    imported and used load_dotenv for dotenv

--- 20_AI/01_llm_api.py
+++ 20_AI/01_llm_api.py
@@ -1,12 +1,15 @@
 import os
-
 from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()  # This specifically looks for a .env file and loads it
+
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY")
 )
 
 response = client.responses.create(
-    model="gpt-5.4",
+    model="gpt-4o",
     input="Write a one-sentence bedtime story about a unicorn."
 )
 
