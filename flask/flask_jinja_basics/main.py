7a452bafdbba851f3ddbedd12cc5114866760f82
omaomach <aaronjoash44@gmail.com>
2026-05-16 00:24:07 +0300

    worked on jinja for flask basics

---
+++ flask/flask_jinja_basics/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    marks = {
+        "John": 45,
+        "Saurabh": 99,
+        "Mark": 55,
+        "Jeff": 88,
+        "Omao": 77
+    }
+    return render_template("index.html", marks=marks)
+
+app.run(debug=True)
