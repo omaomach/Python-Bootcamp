ebcc0b0559cb1e6ae29f4ef689c667a572e70207
omaomach <aaronjoash44@gmail.com>
2026-08-27 10:06:55 +0300

    added some documentation

--- flask/flask_basics/main.py
+++ flask/flask_basics/main.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # We create an instance of the Flask class.
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/") # We then use the route decorate to tell Flask what URL should trigger our function
+def hello_world():# This function then returns the message we want to display in the user's browser. The default content type is HTML, so HTML in the string will be rendered by the browser.
+    return render_template("index.html") # turns index.html template file into HTML sent to the browser: 
 
 app.run(debug=True)
